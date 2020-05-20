1299aff7e9fa5a50835dbfceee12f2b8459e3ef5
SvdStappen <50019396+SvdStappen@users.noreply.github.com>
2020-05-20 17:28:28 +0200

    Updated R package
    Note that I created a new package with a different name (but same working) because some files where lost in the old package. I will try to make the user interface next week (including the opportunity to change edges).

---
+++ SNspread/R/SNspread.R
@@ -0,0 +1,16 @@
+#' This package does X
+#'
+#' The package has several functions.
+#'
+#' @section Create Network:
+#' Creates initial network.
+#'
+#' @section Create Graph:
+#' Visualizes network in graph.
+#'
+#' @section Simulate spread:
+#' Simulates the spread of a disease in a network.
+#'
+#' @docType package
+#' @name SNspread
+NULL
---
+++ SNspread/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(SNspread)
+
+test_check("SNspread")
